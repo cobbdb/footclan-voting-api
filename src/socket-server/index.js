db7f264d259b3f2a878c00d2550b1d3d2dd5ce07
Dan Cobb <cobbdb@gmail.com>
2019-08-03 01:40:36 -0700

    .

--- src/socket-server/index.js
+++ src/socket-server/index.js
@@ -1,10 +1,48 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const WebSocketServer = require('websocket').server;
+const http = require('http');
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+const server = http.createServer((request, response) => {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+});
+
+server.listen(8080, () => {
+  console.log((new Date()) + ` Server is listening on ${server.address().port}`);
+});
+
+const wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: true,
+});
+
+const originIsAllowed = (origin) => {
+  // return origin === 'www.dcobb.media';
+  return true;
+};
+
+wsServer.on('request', (request) => {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  const connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+
+  connection.on('message', (message) => {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    } else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
   });
 
-  ws.send('something');
+  connection.on('close', (reasonCode, description) => {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
